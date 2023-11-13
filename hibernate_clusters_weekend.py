0adc3199141527986e67e029a8f05cef3389821d
Deepak Chourasia <dchouras@redhat.com>
2023-11-13 14:31:29 +0530

    adding support for hypershift hibernation

--- hibernate_clusters_weekend.py
+++ hibernate_clusters_weekend.py
@@ -26,8 +26,38 @@ def get_all_cluster_details(ocm_account:str, clusters:dict):
         clusters.append(oc_cluster(cluster_detail, ocm_account))
     clusters = [cluster for cluster in clusters if cluster.cloud_provider == 'aws']
 
+def get_instances_for_region(region):
+    ec2_client = boto3.client('ec2', region_name=region)
+    filters = [{'Name': 'instance-state-name', 'Values': ['running']}]
+    ec2_map = ec2_client.describe_instances(Filters=filters, MaxResults=1000)
+    ec2_map = [ec2 for ec2 in ec2_map['Reservations']]
+    ec2_map = [instance for ec2 in ec2_map for instance in ec2['Instances']]
+    ec2_map = {list(filter(lambda obj: obj['Key'] == 'Name', instance['Tags']))[0]['Value']: instance for instance in
+               ec2_map}
+    print(region, len(ec2_map))
+    return ec2_map
+
+def get_all_instances(ec2_instances):
+    client = boto3.client('ec2')
+    regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+    for region in regions:
+        ec2_instances[region] = get_instances_for_region(region)
+
+
 def get_cluster_list(ocm_account:str):
     run_command(f'./get_all_cluster_details.sh {ocm_account}')
+
+def hybernate_hypershift_cluster(cluster:oc_cluster, ec2_instances:dict):
+    ec2_map = ec2_instances[cluster.region]
+    print([name for name in ec2_map])
+    worker_nodes = [ec2_name for ec2_name in ec2_map if ec2_name.startswith(f'{cluster.name}-workers-')]
+    ec2_client = boto3.client('ec2', region_name=cluster.region)
+    InstanceIds = [ec2_map[worker_node]['InstanceId'] for worker_node in worker_nodes]
+    print(f'Stopping Worker Instances of cluster {cluster.name}', InstanceIds)
+    ec2_client.stop_instances(InstanceIds=InstanceIds)
+
+
+
 def run_command(command):
     print(command)
     output = os.popen(command).read()
@@ -40,13 +70,16 @@ def hibernate_cluster(cluster: oc_cluster):
 def resume_cluster(cluster: oc_cluster):
     run_command(f'./resume_cluster.sh {cluster.ocm_account} {cluster.id}')
 def main():
+    ec2_instances = {}
+    get_all_instances(ec2_instances)
+
     clusters = []
     ocm_accounts = ['PROD', 'STAGE']
 
     for ocm_account in ocm_accounts:
         get_all_cluster_details(ocm_account, clusters)
 
-    clusters_to_hibernate = [cluster for cluster in clusters if (cluster.type == 'osd' or (cluster.type == 'rosa' and cluster.hcp == 'false')) and cluster.status == 'ready']
+    clusters_to_hibernate = [cluster for cluster in clusters if (cluster.type == 'osd' or (cluster.type == 'rosa')) and cluster.status == 'ready']
     print('cluster to hibernate')
     for cluster in clusters_to_hibernate:
         print(cluster.name, cluster.type)
@@ -54,7 +87,11 @@ def main():
     hibernated_clusters = []
     for cluster in clusters_to_hibernate:
         print('starting with', cluster.name, cluster.type)
-        hibernate_cluster(cluster)
+        if cluster.hcp == "false":
+            # hibernate_cluster(cluster)
+            pass
+        elif cluster.name == 'dchouras-hcp':
+            hybernate_hypershift_cluster(cluster, ec2_instances)
         hibernated_clusters.append(cluster.__dict__)
         # print(f'Hibernated {cluster.name}')
     hibernated_json = json.dumps(hibernated_clusters, indent=4)
