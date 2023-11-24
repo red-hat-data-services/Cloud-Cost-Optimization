0320804de6e19df4276eb255804d1efe46704619
Deepak Chourasia <dchouras@redhat.com>
2023-11-24 10:35:47 +0530

    adding ebs cleanup with hibernation

--- src/check_instances_status.py
+++ src/check_instances_status.py
@@ -111,7 +111,7 @@ def check_instance_status(cluster:oc_cluster, ec2_running_map:dict, ec2_stopped_
         current_stopped_instances = [instance['InstanceId'] for ec2 in current_stopped_instances for instance in
                                      ec2['Instances']]
         if current_stopped_instances:
-            print(f'Stopping Running Worker Instances of cluster {cluster.name}', current_stopped_instances)
+            print(f'Stopping Running Worker Instances of cluster {cluster.name}', InstanceIds_running)
             ec2_client.stop_instances(InstanceIds=InstanceIds_running)
             print(f'Started hibernating the cluster {cluster.name}')
 
