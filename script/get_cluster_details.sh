d4cd5f2f7e980b1b45417653271dbfeb889bdcc8
Deepak Chourasia <dchouras@redhat.com>
2023-12-01 16:53:42 +0530

    adding additional cluster info

--- script/get_cluster_details.sh
+++ script/get_cluster_details.sh
@@ -21,7 +21,6 @@ creator_id=$(ocm get /api/accounts_mgmt/v1/subscriptions -p search="cluster_id='
 creator_name=$(ocm get /api/accounts_mgmt/v1/accounts/${creator_id} | jq -r '.first_name + " " + .last_name')
 creator_email=$(ocm get /api/accounts_mgmt/v1/accounts/${creator_id} | jq -r '.email')
 
-cd script
 cp cluster_details.json ${cluster_id}_details.json
 sed -i "s,cluster_id,${cluster_id},g" ${cluster_id}_details.json
 sed -i "s,cluster_creation_date,${creation_date},g" ${cluster_id}_details.json
