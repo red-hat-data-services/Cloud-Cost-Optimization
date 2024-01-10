651ee9eaade97e78d7a9336ef0563bb618e6bdf5
Deepak Chourasia <dchouras@redhat.com>
2024-01-10 14:28:26 +0530

    adding reminders to update inactive hours

--- src/cluster_aggregator.py
+++ src/cluster_aggregator.py
@@ -186,7 +186,8 @@ def update_smartsheet_data(clusters:dict[oc_cluster]):
             sheet = smart.Sheets.get_sheet(sheed_id)
             newRows = [row for row in sheet.rows if row.id in new_row_ids]
             for row in newRows:
-                send_request_to_update_inactive_hours(row, column_map, smart)
+                if (not row.cells[5].value or ':' not in row.cells[5].value) and row.cells[1].value == 'dchouras':
+                    send_request_to_update_inactive_hours(row, column_map, smart)
 
 
     if smartsheet_deleted_data:
