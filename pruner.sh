2080db02593af5ab704b8ad65ea3a0c3cd900087
Christopher Kodama <ckodama@redhat.com>
2025-01-02 17:03:13 -0500

    replace xargs with xargs -r for gnu compatibility

--- pruner.sh
+++ pruner.sh
@@ -33,54 +33,54 @@ for OLD_JOB in $OLD_JOBS; do
 
   echo "finding and deleting compute instance-related resources..."
   INSTANCES=$(gcloud compute instances list --filter "name ~ $OLD_JOB" --format json)
-  echo "$INSTANCES" | format_for_deletion zone | xargs -L 1 gcloud compute instances delete --quiet
+  echo "$INSTANCES" | format_for_deletion zone | xargs -r -L 1 gcloud compute instances delete --quiet
 
   DISKS=$(gcloud compute disks list --format json --filter "labels: $OLD_JOB" --format json)
-  echo "$DISKS" | format_for_deletion zone | xargs -L 1 gcloud compute disks delete --quiet
+  echo "$DISKS" | format_for_deletion zone | xargs -r -L 1 gcloud compute disks delete --quiet
 
   FWD_RULES=$(gcloud compute forwarding-rules list --filter "name ~ $OLD_JOB" --format json)
-  echo "$FWD_RULES" | format_for_deletion region | xargs -L 1 gcloud compute forwarding-rules delete --quiet
+  echo "$FWD_RULES" | format_for_deletion region | xargs -r -L 1 gcloud compute forwarding-rules delete --quiet
 
   BACKEND_SVC=$(gcloud compute backend-services list --filter "name ~ $OLD_JOB" --format json)
-  echo "$BACKEND_SVC" | format_for_deletion region | xargs gcloud compute backend-services delete --quiet
+  echo "$BACKEND_SVC" | format_for_deletion region | xargs -r gcloud compute backend-services delete --quiet
 
   INST_GROUPS=$(gcloud compute instance-groups list --filter "name ~ $OLD_JOB" --format json)
-  echo "$INST_GROUPS" |  format_for_deletion zone | xargs -L 1 gcloud compute instance-groups unmanaged delete --quiet
+  echo "$INST_GROUPS" |  format_for_deletion zone | xargs -r -L 1 gcloud compute instance-groups unmanaged delete --quiet
 
   echo "finding and deleting networking-related resources..."
   FW_RULES=$(gcloud compute firewall-rules list --format json --filter "network ~ $OLD_JOB" | jq -r '.[].name')
-  echo "$FW_RULES" | xargs gcloud compute firewall-rules delete --quiet
+  echo "$FW_RULES" | xargs -r gcloud compute firewall-rules delete --quiet
 
   ROUTERS=$(gcloud compute routers list --filter "name ~ $OLD_JOB" --format json) 
-  echo "$ROUTERS" |  format_for_deletion region | xargs -L 1 gcloud compute routers delete --quiet
+  echo "$ROUTERS" |  format_for_deletion region | xargs -r -L 1 gcloud compute routers delete --quiet
 
   FWD_RULES=$(gcloud compute forwarding-rules list --filter "name ~ $OLD_JOB" --format json)
-  echo "$FWD_RULES" | format_for_deletion region | xargs -L 1 gcloud compute forwarding-rules delete --quiet
+  echo "$FWD_RULES" | format_for_deletion region | xargs -r -L 1 gcloud compute forwarding-rules delete --quiet
 
   ADDRS=$(gcloud compute addresses list --filter "name ~ $OLD_JOB" --format json) 
-  echo "$ADDRS" | format_for_deletion region | xargs -L 1 gcloud compute addresses delete --quiet
+  echo "$ADDRS" | format_for_deletion region | xargs -r -L 1 gcloud compute addresses delete --quiet
 
   SUBNETS=$(gcloud compute networks subnets list --filter "name ~ $OLD_JOB" --format json)
-  echo "$SUBNETS" | format_for_deletion region | xargs -L 1 gcloud compute networks subnets delete --quiet
+  echo "$SUBNETS" | format_for_deletion region | xargs -r -L 1 gcloud compute networks subnets delete --quiet
 
   HEALTH_CHECKS=$(gcloud compute health-checks list --filter "name ~ $OLD_JOB" --format json)
-  echo "$HEALTH_CHECKS" |  jq -r '.[].name' | xargs gcloud compute health-checks delete --quiet
+  echo "$HEALTH_CHECKS" |  jq -r '.[].name' | xargs -r gcloud compute health-checks delete --quiet
 
   TARGET_POOLS=$(gcloud compute target-pools list --filter "name ~ $OLD_JOB" --format json)
-  echo "$TARGET_POOLS" | format_for_deletion region  | xargs -L 1 gcloud compute target-pools delete --quiet
+  echo "$TARGET_POOLS" | format_for_deletion region  | xargs -r -L 1 gcloud compute target-pools delete --quiet
 
   NETWORKS=$(gcloud compute networks list --filter "name ~ $OLD_JOB" --format json | jq -r '.[].name')
-  echo "$NETWORKS" | xargs gcloud compute networks delete --quiet
+  echo "$NETWORKS" | xargs -r gcloud compute networks delete --quiet
 
   echo "finding and deleting service accounts..."
   SVC_ACCTS=$(gcloud iam service-accounts list --filter "displayName ~ $OLD_JOB" --format json | jq -r '.[].email')
-  echo "$SVC_ACCTS" | xargs -L 1 gcloud iam service-accounts delete --quiet
+  echo "$SVC_ACCTS" | xargs -r -L 1 gcloud iam service-accounts delete --quiet
 
   echo "finding and deleting storage buckets..."
   BUCKETS=$(gcloud storage buckets list --filter "name ~ $OLD_JOB" --format json)
-  OBJECTS=$(echo "$BUCKETS" | jq -r '.[].storage_url' | xargs gcloud storage objects list --format json)
-  echo "$OBJECTS" | jq -r '.[].storage_url' | xargs gcloud storage rm --quiet
-  echo "$BUCKETS" | jq -r '.[].storage_url' | xargs gcloud storage buckets delete --quiet
+  OBJECTS=$(echo "$BUCKETS" | jq -r '.[].storage_url' | xargs -r gcloud storage objects list --format json)
+  echo "$OBJECTS" | jq -r '.[].storage_url' | xargs -r gcloud storage rm --quiet
+  echo "$BUCKETS" | jq -r '.[].storage_url' | xargs -r gcloud storage buckets delete --quiet
 
   # Delete zones last because we are leveraging it early to get the entire list of jobs. 
   # If we deleted them first and there's an error later, we wouldn't be able to get back the whole list of jobs
@@ -88,7 +88,7 @@ for OLD_JOB in $OLD_JOBS; do
   DNS_ZONE=$(gcloud dns managed-zones list --filter "name ~ $OLD_JOB" --format json | jq -r '.[].name')
   if [ -n "$DNS_ZONE" ] ; then
     DNS_RECORDS=$(gcloud dns record-sets list --zone "$DNS_ZONE" --format json)
-    echo "$DNS_RECORDS" | jq -r '.[] | select(.type=="A") | .name' | xargs -L 1 gcloud dns record-sets delete --zone "$DNS_ZONE" --type A 
+    echo "$DNS_RECORDS" | jq -r '.[] | select(.type=="A") | .name' | xargs -r -L 1 gcloud dns record-sets delete --zone "$DNS_ZONE" --type A 
     gcloud dns managed-zones delete "$DNS_ZONE"
   fi
  
